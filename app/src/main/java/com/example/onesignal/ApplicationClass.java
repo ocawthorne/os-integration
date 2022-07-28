28a3576909e2e1f78025939b13175ff9fc3d39fb
ocawthorne <ocwthrn+github@gmail.com>
2022-07-28 01:56:22 +0100

    Add ApplicationClass.java

--- app/src/main/java/com/example/onesignal/ApplicationClass.java
+++ app/src/main/java/com/example/onesignal/ApplicationClass.java
@@ -1,2 +1,16 @@
-package com.example.onesignal;public class ApplicationClass {
+package com.example.onesignal;
+
+import com.onesignal.OneSignal;
+import android.app.Application;
+
+public class ApplicationClass extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId("6dc641c3-6ebc-4d9d-b38a-80482db056ea");
+    }
 }
